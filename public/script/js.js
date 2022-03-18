d3e1bc8e3a4e0a8689d4ff0cae00eb02b8d50721
Igor Costiuc <gast777@yahoo.com>
2022-03-18 20:01:34 +0200

    leson 3: methods of chosing elements

--- public/script/js.js
+++ public/script/js.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {
 
-    $("h1").addClass("selected");
+    // methods of chosing elements
+    // tags by parent
+    //$(".example").parent()
+    //cloest element by id and tag example
+    //$("li#second_list_item").closest("ul").addClass("selected");
+    //$('#second_list_item').closest("li").addClass("selected");
+    //$(".green").closest("li").addClass("selected");
+
+    //parents
+    //$("#second_list_item").parents().addClass("selected");
+    //parent 
+    //$("#second_list_item").parent().addClass("selected");
+
+    //find
+    //$("ul").find(".green").hide();
+
+    //prev
+    //$(".green").prev().addClass("selected");
+
+    //next
+    //$("#second_list_item").next().addClass("selected");
+
+    //siblings
+    $("#second_list_item").siblings(".green").addClass("selected");
+
 
 });
