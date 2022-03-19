8aad0a29d3fe1f8715f286df41601b035b15423d
Igor Costiuc <gast777@yahoo.com>
2022-03-19 21:39:47 +0200

    leson 6: basic calculator

--- public/script/js.js
+++ public/script/js.js
@@ -1,50 +1,27 @@
 $(document).ready(function() {
 
-    //animate() - change CSS proprety with animation
-    //animate({CSS props}), duration in ms)
-    $("#customWidth").on("click", function() {
-        $("#testBox").animate({ "width": "200px" }, 300);
-        $("#testBox").animate({ "height": "200px" }, 300);
+    $("#formSum").on("submit", function(event) {
+        event.preventDefault();
 
+        let numberOne,
+            numberTwo,
+            summ;
+
+        numberOne = $("#numberOne").val();
+        numberTwo = $("#numberTwo").val();
+
+        numberOne = parseInt(numberOne);
+        numberTwo = parseInt(numberTwo);
+
+
+        if (isNaN(numberOne, numberTwo)) {
+            numberOne = 0
+            numberTwo = 0
+        };
+
+        summ = numberOne + numberTwo;
+
+        $("#sumResult").text(summ);
     });
 
-    $("#customOpacity").on("click", function() {
-        $("#testBox").animate({ "opacity": 0 }, 1000)
-    });
-
-    $("#hide").on("click", function() {
-        $("#testBox").hide(700)
-    });
-
-    $("#show").on("click", function() {
-        $("#testBox").show("slow")
-    });
-
-    $("#hideAndShow").on("click", function() {
-        $("#testBox").hide("1000").delay("100").show("1000")
-    });
-
-    $("#fadeOut").on("click", function() {
-        $("#testBox").fadeOut("1000")
-    });
-
-    $("#fadeIn").on("click", function() {
-        $("#testBox").fadeIn("1000")
-    });
-
-    $("#fadeToggle").on("click", function() {
-        $("#testBox").fadeToggle(1000);
-    });
-
-    $("#slideUp").on("click", function() {
-        $("#testBox").slideUp(1000);
-    });
-
-    $("#slideDown").on("click", function() {
-        $("#testBox").slideDown(1000);
-    });
-
-    $("#slideToggle").on("click", function() {
-        $("#testBox").slideToggle(1000);
-    });
 });
