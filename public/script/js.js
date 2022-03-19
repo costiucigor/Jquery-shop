041dbde41798446a92c73859bbafc22043cf26ae
Igor Costiuc <gast777@yahoo.com>
2022-03-19 21:05:13 +0200

    leson 5: working with animations

--- public/script/js.js
+++ public/script/js.js
@@ -1,18 +1,50 @@
 $(document).ready(function() {
 
-    $("#addGreen").on("click", function() {
-        $("#textbox").addClass("greenBg");
+    //animate() - change CSS proprety with animation
+    //animate({CSS props}), duration in ms)
+    $("#customWidth").on("click", function() {
+        $("#testBox").animate({ "width": "200px" }, 300);
+        $("#testBox").animate({ "height": "200px" }, 300);
+
     });
 
-    $("#removeGreen").on("click", function() {
-        $("#textbox").removeClass("greenBg");
+    $("#customOpacity").on("click", function() {
+        $("#testBox").animate({ "opacity": 0 }, 1000)
     });
 
-    $("#toggleGreen").on("click", function() {
-        $("#textbox").toggleClass("greenBg");
+    $("#hide").on("click", function() {
+        $("#testBox").hide(700)
     });
-    // bad practice!!
-    $("#customCSS").on("click", function() {
-        $("#textbox").css("border", "5px solid red")
-    })
+
+    $("#show").on("click", function() {
+        $("#testBox").show("slow")
+    });
+
+    $("#hideAndShow").on("click", function() {
+        $("#testBox").hide("1000").delay("100").show("1000")
+    });
+
+    $("#fadeOut").on("click", function() {
+        $("#testBox").fadeOut("1000")
+    });
+
+    $("#fadeIn").on("click", function() {
+        $("#testBox").fadeIn("1000")
+    });
+
+    $("#fadeToggle").on("click", function() {
+        $("#testBox").fadeToggle(1000);
+    });
+
+    $("#slideUp").on("click", function() {
+        $("#testBox").slideUp(1000);
+    });
+
+    $("#slideDown").on("click", function() {
+        $("#testBox").slideDown(1000);
+    });
+
+    $("#slideToggle").on("click", function() {
+        $("#testBox").slideToggle(1000);
+    });
 });
