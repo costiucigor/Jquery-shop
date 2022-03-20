b58195699c91c211379d3dea5c216e38146b18ad
Igor Costiuc <gast777@yahoo.com>
2022-03-20 11:32:43 +0200

    ultimate task

--- public/script/js.js
+++ public/script/js.js
@@ -1,36 +1,68 @@
 $(document).ready(function() {
 
-    //1. getting the value of the src of img
-    //let srcValue = $("#imgHolder img").attr("src");
+    let modelSpecs,
+        modelPrice,
+        modelSpecsHolder,
+        modelPriceHolder;
 
+    modelSpecsHolder = $("#modelSpecs");
+    modelPriceHolder = $("#modelPrice");
 
-    //2. change the attribute value of the image
-    // $("#imgHolder img").on("click", function() {
-    //     $(this).attr("src", "img/blueBMW.png");
+    modelPrice = 0;
+    modelSpecs = "";
+
+    const calculatePrice = () => {
+        let modelPriceEngine = $("input[name=engine]:checked", "#autoForm").val();
+        let modelPriceTransmission = $("input[name=transmission]:checked", "#autoForm").val();
+        let modelPricePackage = $("input[name=package]:checked", "#autoForm").val();
+
+        modelPriceEngine = parseInt(modelPriceEngine);
+        modelPriceTransmission = parseInt(modelPriceTransmission);
+        modelPricePackage = parseInt(modelPricePackage);
+
+        modelPrice = modelPriceEngine + modelPriceTransmission + modelPricePackage;
+
+        modelPriceHolder.text(modelPrice + " lei");
+    };
+
+    const compileSpecs = () => {
+        modelSpecs = $("input[name=engine]:checked + label", "#autoForm").text();
+        modelSpecs = modelSpecs + ", " + $("input[name=transmission]:checked + label", "#autoForm").text();
+        modelSpecs = modelSpecs + ", " + $("input[name=package]:checked + label", "#autoForm").text();
+        modelSpecsHolder.text(modelSpecs);
+    };
+
+    const changeColor = () => {
+        $("#colorsSelector .colorItem").on("click", function() {
+            let imgPath = $(this).attr("data-img-path");
+            $("#imgHolder img").attr("src", imgPath);
+        });
+    }
+
+    $("#autoForm input").on("change", function() {
+        calculatePrice();
+        compileSpecs();
+    });
+    calculatePrice();
+    compileSpecs();
+    changeColor();
+    //not working
+    // let currencyUrl = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%3D%22USDINR%2C%20EURINR%2CCADINR%2CJPYINR%2CGBPINR%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+    // let rurUsdRate = 0;
+
+    // $.ajax({
+    //     url: currencyUrl,
+    //     cache: false,
+    //     succes: function(html) {
+    //         console.log(html.query.results.rate[0].rate);
+    //         rurUsdRate = html.query.results.rate[0].rate;
+    //         calculateUSD();
+    //     }
     // });
 
-    //3. separate action for each colour
-    // $("#colorsSelector .colorItem.colorBlue").on("click", function() {
-    //     $("#imgHolder img").attr("src", "img/blueBMW.png");
-    // });
+    // const calculateUSD = () => {
+    //     let modelPriceUSD = modelPrice / rurUsdRate;
+    //     alert(modelPriceUSD);
+    // };
 
-    // $("#colorsSelector .colorItem.colorWhite").on("click", function() {
-    //     $("#imgHolder img").attr("src", "img/whiteBMW.png");
-    // });
-
-    // $("#colorsSelector .colorItem.colorBlack").on("click", function() {
-    //     $("#imgHolder img").attr("src", "img/blackBMW.png");
-    // });
-
-    //4. one function for all colours
-
-    $("#colorsSelector .colorItem").on("click", function() {
-        let imgPath;
-
-        imgPath = $(this).attr("data-img-path");
-        $("#imgHolder img").attr("src", imgPath);
-    })
-
-
-
-})
+});
