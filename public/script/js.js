ee6578d8947b5c7c31484f906d91eb070699d6a7
Igor Costiuc <gast777@yahoo.com>
2022-03-20 08:53:36 +0200

    leson 6: working with atributes

--- public/script/js.js
+++ public/script/js.js
@@ -1,31 +1,36 @@
 $(document).ready(function() {
 
-    $("#formSum").on("submit", function(event) {
-        event.preventDefault();
-
-        let numberOne,
-            numberTwo,
-            summ;
-
-        numberOne = $("#numberOne").val();
-        numberTwo = $("#numberTwo").val();
-
-        numberOne = parseInt(numberOne);
-        numberTwo = parseInt(numberTwo);
+    //1. getting the value of the src of img
+    //let srcValue = $("#imgHolder img").attr("src");
 
 
-        if (Number.isNaN(numberOne)) {
-            numberOne = 0;
-        };
+    //2. change the attribute value of the image
+    // $("#imgHolder img").on("click", function() {
+    //     $(this).attr("src", "img/blueBMW.png");
+    // });
 
-        if (Number.isNaN(numberTwo)) {
-            numberTwo = 0;
-        };
+    //3. separate action for each colour
+    // $("#colorsSelector .colorItem.colorBlue").on("click", function() {
+    //     $("#imgHolder img").attr("src", "img/blueBMW.png");
+    // });
+
+    // $("#colorsSelector .colorItem.colorWhite").on("click", function() {
+    //     $("#imgHolder img").attr("src", "img/whiteBMW.png");
+    // });
+
+    // $("#colorsSelector .colorItem.colorBlack").on("click", function() {
+    //     $("#imgHolder img").attr("src", "img/blackBMW.png");
+    // });
+
+    //4. one function for all colours
+
+    $("#colorsSelector .colorItem").on("click", function() {
+        let imgPath;
+
+        imgPath = $(this).attr("data-img-path");
+        $("#imgHolder img").attr("src", imgPath);
+    })
 
 
-        summ = numberOne + numberTwo;
 
-        $("#sumResult").text(summ);
-    });
-
-});
+})
